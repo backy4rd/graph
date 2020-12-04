077491b02fcff101bd92a0b2d9ca37e2bc6ce433
backy4rd <khainguyen9209@gmail.com>
2020-12-04 20:58:34 +0700

    update thi_cong

--- buoi4/thi_cong.c
+++ buoi4/thi_cong.c
@@ -1,32 +1,31 @@
 #include <stdio.h>
 #include "../graph/ds_cung_cohuong.c"
-#include "../data_structure/stack.c"
 #include "../data_structure/list.c"
 #define INFINITY 9999999
 
 void ranking_graph(Graph graph, int rank[]) {
     int degs[graph.n + 1];
-    Stack s1, s2;
-    make_null_stack(&s1);
+    List s1, s2;
+    make_null_list(&s1);
 
     for (int i = 1; i <= graph.n; i++) {
         degs[i] = in_degree(graph, i);
         if (degs[i] == 0) {
-            push(&s1, i);
+            push_back(&s1, i);
         }
     }
 
-    for (int current_rank = 0; !is_stack_empty(s1); current_rank++) {
-        make_null_stack(&s2);
-        while (!is_stack_empty(s1)) {
-            int u = pop(&s1);
+    for (int current_rank = 0; !is_list_empty(s1); current_rank++) {
+        make_null_list(&s2);
+        for (int i = 1; i <= s1.size; i++) {
+            int u = element_at(s1, i);
             rank[u] = current_rank;
 
             for (int v = 1; v <= graph.n; v++) {
                 if (adjacent(graph, u, v)) {
                     degs[v]--;
                     if (degs[v] == 0) {
-                        push(&s2, v);
+                        push_back(&s2, v);
                     }
                 }
             }
@@ -41,10 +40,7 @@ List topo_sort(int rank[], int n) {
     List topo;
     make_null_list(&topo);
 
-    for (int i = 1; i <= n; i++) {
-        if (highest_rank < rank[i]) highest_rank = rank[i];
-    }
-    for (int i = 0; i <= highest_rank; i++) {
+    for (int i = 0; topo.size < n; i++) {
         for (int v = 1; v <= n; v++) {
             if (rank[v] == i) {
                 push_back(&topo, v);
@@ -55,23 +51,31 @@ List topo_sort(int rank[], int n) {
     return topo;
 }
 
-void organize_construction(Graph graph, int d[], int rank[], int earliest[], int latest[]) {
+void organize_construction(Graph graph, int d[], int earliest[], int latest[]) {
     Graph temp = graph;
+    temp.n += 2;
+
+    int alpha = graph.n + 1;
+    int beta = graph.n + 2;
+    int rank[graph.n + 2];
+
+    ranking_graph(graph, rank);
 
     // add alpha and beta
     for (int i = 1; i <= graph.n; i++) {
         if (in_degree(graph, i) == 0) {
-            add_edge(&temp, 0, i);
+            add_edge(&temp, alpha, i);
         }
         if (out_degree(graph, i) == 0) {
-            add_edge(&temp, i, graph.n + 1);
+            add_edge(&temp, i, beta);
         }
 
     }
 
-    d[0] = 0; // alpha job
     List topo = topo_sort(rank, graph.n);
-    push_back(&topo, graph.n + 1); // insert beta job as hight rank
+
+    earliest[alpha] = latest[alpha] = d[alpha] = 0; // alpha job
+    push_back(&topo, beta); // insert beta job as highest rank
 
     // calculate earliest start time
     for (int i = 1; i <= topo.size; i++) {
@@ -124,16 +128,12 @@ int main() {
 
     int rank[n + 1], earliest[n + 2], latest[n + 2];
 
-    ranking_graph(G, rank);
-    organize_construction(G, d, rank, earliest, latest);
+    organize_construction(G, d, earliest, latest);
 
-    printf("%d\n", earliest[n + 1]); // earliest time to complete the construction
-    for (int i = 1; i <= n; i++) {
+    printf("%d\n", earliest[n + 2]);
+    for (int i = 1; i <= n + 2; i++) {
         printf("%d-%d\n", earliest[i], latest[i]);
     }
-    printf("0-0\n"); // print alpha
-    printf("%d-%d\n", earliest[n + 1], latest[n + 1]); // print beta
 
-    printf("\n");
     return 0;
 }
